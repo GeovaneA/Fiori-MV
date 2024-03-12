47b7c8ad17d08c598d984ebeabdae6cbaf3fee80
Geovane <geovane.alborghetti@gmail.com>
2024-03-12 17:35:44 -0300

    v1.1
    novos ajustes

--- Fiori Elements - Dev/zui5.relatorio.base.conhecimento/webapp/ext/controller/ListReportExt.controller.js
+++ Fiori Elements - Dev/zui5.relatorio.base.conhecimento/webapp/ext/controller/ListReportExt.controller.js
@@ -1,4 +1,5 @@
 sap.ui.define([
+    "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast"
 ], function(MessageToast) {
     'use strict';
@@ -10,25 +11,36 @@ sap.ui.define([
             //MessageToast.show("Botão editar");
             this._getSelectedRow();
 
-            if(this._getModel("movelView").getProperty("/Userid") != undefined )
-            
+            if(this._getModel("modelView").getProperty("/Userid") != undefined )
+                this._onOpenDialog() 
+            // alert("teste")
             else 
-              alert("Nenhum item selecionado");
-
+                MessageToast.show("Nenhum item selecionado")
         },
 
         _getModel: function(sModel){
-            return this.getView()._getModel(sModel);
+            return this.getView().getModel(sModel);
         },
         _getSelectedRow: function(){
-            this.sPath = this.getView().byId("listReport").getTable()._getSelectedContextPaths()[0];
+            this.sPath = this.getView().byId("listReport").getTable().getSelectedContextPaths()[0];
             var sEmail = this._getModel().getProperty(this.sPath + "/Email");
+            var sUserid = this._getModel().getProperty(this.sPath + "/Userid");
 
-            this._getModel("modelView").setProperty("/Userid", this._getModel("modelView").getProperty(this.sPath + "/Userid"));
+        //    this._getModel("modelView").setProperty("/Userid", this._getModel("modelView").getProperty(this.sPath + "/Userid"));
 
+           this._getModel("modelView").setProperty("/Userid", sUserid);
             this._getModel("modelView").setProperty("/Email", sEmail);
 
-            this._getModel("modelView").setProperty("/Fullname", this._getModel("modelView").getProperty(this.sPath + "/Fullname"));
+            this._getModel("modelView").setProperty("/Fullname", this._getModel().getProperty(this.sPath + "/Fullname"));
+        },
+
+        _onOpenDialog() {
+            // create dialog lazily
+            this.pDialog ??= this.loadFragment({
+                name: "com.moovi.zui5.relatorio.base.conhecimento.fragment.DialogEdit"
+            });
+        
+            this.pDialogEdit.then((oDialog) => oDialog.open());
         }
     };
 });
