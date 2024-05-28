7c03ee84c46227f833c0d8cb8b53921b83bfabc4
Geovane Alborghetti <geovane.alborghetti@gmail.com>
2024-05-27 23:06:54 -0300

    Dialog
    fazendo dialog

---
+++ zui5.relatorio.base.pessoa/README.md
@@ -0,0 +1,43 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Sun May 05 2024 20:10:38 GMT-0300 (Horário Padrão de Brasília)|
+|**App Generator**<br>@sap/generator-fiori-elements|
+|**App Generator Version**<br>1.13.3|
+|**Generation Platform**<br>Visual Studio Code|
+|**Template Used**<br>List Report Page V2|
+|**Service Type**<br>SAP System (ABAP On Premise)|
+|**Service URL**<br>http://ndc-srvhana.opus-idc.com.br:8000//sap/opu/odata/sap/ZGSACDS_RPT_PESSOACID_CDS
+|**Module Name**<br>zui5.relatorio.base.pessoa|
+|**Application Title**<br>Base Pessoa|
+|**Namespace**<br>com.moovi|
+|**UI5 Theme**<br>sap_horizon|
+|**UI5 Version**<br>1.114.0|
+|**Enable Code Assist Libraries**<br>False|
+|**Enable TypeScript**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Main Entity**<br>zgsacds_rpt_pessoacid|
+
+## zui5.relatorio.base.pessoa
+
+Base pessoa
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+- It is also possible to run the application using mock data that reflects the OData Service URL supplied during application generation.  In order to run the application with Mock Data, run the following from the generated app root folder:
+
+```
+    npm run start-mock
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ zui5.relatorio.base.pessoa/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,63 @@
+sap.ui.define([
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment",
+    "sap/ui/core/mvc/Controller"
+], function(MessageToast) {
+    'use strict';
+
+    return {
+
+        sPath: "",
+        onEdit: function(oEvent) {
+            this._getSelectedRow();
+            if(this._getModel("modelView").getProperty("/Idperson") != undefined)
+                this._getValueHelpRequest();
+            
+            //else
+
+
+        },
+
+        _getModel: function(sModel){
+            return this.getView().getModel(sModel);
+        },
+
+        _getSelectedRow: function(){
+
+            this.sPath = this.getView().byId("listReport").getTable().getSelectedContextPaths()[0];
+            var sName = this._getModel().getProperty(this.sPath + "/Fullname");
+            this._getModel("modelView").setProperty("/Idperson", this._getModel().getProperty(this.sPath + "/Idperson"))
+
+            this._getModel("modelView").setProperty("/Fullname", sName);
+        },
+
+        _getValueHelpRequest: function(){
+            var oView = this.getView();
+
+            if (!this._pValueHelpDialog){
+                this._pValueHelpDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "com.moovi.zui5.relatorio.base.pessoa.fragment.DialogEdit",
+                    controller: this
+                }).then(function (oValueHelpDialog){
+                    oView.addDependent(oValueHelpDialog);
+                    return oValueHelpDialog;
+                });
+            }
+
+            this._pValueHelpDialog.then(function(oValueHelpDialog){
+                oValueHelpDialog.open();
+            }.bind(this));
+            
+        },
+
+        onSave: function(){
+            this._getModel().setProperty(this.sPath + "/Idperson", this._getModel("modelView").getProperty("/Idperson"));
+        },
+
+        onClose: function(){
+            this.oDialog = this.getView().byId("ListDialog");
+            this.oDialog.close();
+        }
+    };
+});
